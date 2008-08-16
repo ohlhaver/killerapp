1cd2f7ac8411e58b23c4a189af3f20158241432b
Justus  Ohlhaver <ohlhaver@gmail.com>
2008-08-16 16:42:49 +0300

    re added manual login on top of automatic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,30 @@
 class SessionsController < ApplicationController
   def create 
+     @current_user = User.find_by_login_and_password(params[:login], params[:password])
+
+      if @current_user.login
+        session[:user_id] = @current_user.id
+        if session[:return_to]
+          redirect_to session[:return_to]
+          session[:return_to] = nil
+        else    
+        redirect_to rawstories_path
+        end
+      else
+        render :action => 'new'
+      end
   end
 
   def new
-    
-    @current_user = User.create
-    session[:user_id] = @current_user.id
-        redirect_to rawstories_path
+    unless logged_in?
+      @current_user = User.create
+      session[:user_id] = @current_user.id
+      redirect_to rawstories_path 
+    end    
   end
 
   def destroy
-    session[:user_id] = @current_user = nil
+    session[:user_id] = @current_user = User.create
   end
 
 end
