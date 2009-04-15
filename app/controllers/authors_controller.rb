1ae9385a0b6c621399eb2394cf15893b26706d45
Justus  Ohlhaver <ohlhaver@gmail.com>
2009-04-16 00:59:36 +0200

    fixed the problem with iphone detection and deactivated author/view and fixed something in about

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,7 +8,8 @@ class AuthorsController < ApplicationController
     unless read_fragment({:l => @l, :id => params[:id], :page => params[:page] || 1}) 
         
         @author = Author.find(params[:id])
-        @rawstories_published = @author.rawstories.find(:all, :order => 'rawstories.id DESC')
+        @rawstories_published = [] # deactivated this for now
+        #@rawstories_published = @author.rawstories.find(:all, :order => 'rawstories.id DESC')
         @rawstories_published = @rawstories_published.paginate :page => params[:page],
                                             :per_page => 5
     end
