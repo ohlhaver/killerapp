9f52bc155918a0a1c6e01ea383618f676d821d57
Jagadish Rath <jrath95@gmail.com>
2009-06-08 16:03:05 +0530

    Commented out protect from forgery before filter in appliaction.rb

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '30f91683eea1d7b3b2b38a0fba2e45ba'
+  #protect_from_forgery # :secret => '30f91683eea1d7b3b2b38a0fba2e45ba'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
