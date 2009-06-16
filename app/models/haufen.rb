1456d3b1334cb51d27895e0ed5ae22ed6853c660
Jagadish Rath <jrath95@gmail.com>
2009-06-16 16:59:22 +0530

    showing images for haufens

--- app/models/haufen.rb
+++ app/models/haufen.rb
@@ -2,4 +2,8 @@ class Haufen < ActiveRecord::Base
   belongs_to :hsession
   has_many :rawstories
   has_one  :haufens_story_image
+
+  def image_story
+    image_story_id == 0 ? nil : Rawstory.find(image_story_id)
+  end
 end
