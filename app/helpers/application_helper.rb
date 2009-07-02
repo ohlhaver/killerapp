9769a155d0d56036b732fb8d09fb22c68805dd3b
Jagadish Rath <jrath95@gmail.com>
2009-07-02 22:27:28 +0530

    app/helpers/application_helper.rb changes for via : saving space for twitter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def via_jurnalo_label(language=nil)
-   " ( found via www.jurnalo.#{language == 2 ? 'de' : 'com'} ) "
+   " (found via www.jurnalo.#{language == 2 ? 'de' : 'com'})"
   end
   def invite_label
     if @language ==2
