e49d8ffc820f480a3af102f059aace267aa3fc8e
Jagadish Rath <jrath95@gmail.com>
2009-07-29 22:26:44 +0530

    showing the authors and sub categories in sorted order

--- app/controllers/site_map_controller.rb
+++ app/controllers/site_map_controller.rb
@@ -6,14 +6,22 @@ class SiteMapController < ApplicationController
   end
   def authors_in_category
     @category = SiteMapAuthorCategory.find(params[:id])
-    @sub_categories = SiteMapAuthorSubCategory.find(:all, :conditions => ["category_id = ?", params[:id].to_i])
+    @sub_categories = SiteMapAuthorSubCategory.find(:all, :conditions => ["category_id = ?", params[:id].to_i], :order => "id ASC")
   end
   def authors_in_sub_category
     @sub_category = SiteMapAuthorSubCategory.find(params[:id])
     @category = SiteMapAuthorCategory.find(@sub_category.category_id) rescue nil
-    author_ids = SiteMapCategoryAuthorMap.find(:all, :conditions => ["sub_category_id = ?", params[:id].to_i], :select => "author_id").collect{|am| am.author_id}*","
+    c_a_maps = SiteMapCategoryAuthorMap.find(:all, 
+                                             :conditions => ["sub_category_id = ?", params[:id].to_i],
+                                             :select => "author_id")
+    rank = {}
+    c_a_maps.each_with_index do |am,i|
+      rank[am.author_id] = i
+    end
+    author_ids = c_a_maps.collect{|am| am.author_id}*","
     @authors = []
     return if author_ids.blank?
     @authors = Author.find(:all, :select => "name,id", :conditions => "id IN ( #{ author_ids} )")
+    @authors = @authors.sort_by{|a| rank[a.id]}
   end
 end
